0b6ec660a76309038e445b0a7130b795ce86a52d
Bnmpower <bnmpowerislove@gmail.com>
2025-11-14 04:08:24 -0800

    Created home page

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from homePage import homeBP
+import requests
 
 app = Flask(__name__)
 
-# Members API ROUTE
-@app.route("/members")
+#Test
+@app.route("/helloWorld")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return "Hello World"
 
+app.register_blueprint(homeBP)
 
 if __name__ == "__main__":
     app.run(debug=True)
