845f94562155713dae6a2462ceb2536bd37d9403
dkim515-gif <dkim515@ucr.edu>
2025-11-14 16:18:57 -0800

    created ratings for anime

--- flask-server/homePage.py
+++ flask-server/homePage.py
@@ -8,10 +8,17 @@ def trendingAnime():
     urlTrending = restAPI + "top/anime?filter=airing"
     return requests.get(urlTrending).json()
 
+def ratingsAnime():
+    anime_id = 21
+    urlRating = f"{restAPI}anime/{anime_id}/statistics"
+    return requests.get(urlRating).json()
+
 @homeBP.route("/home")
 def homePage():
     trendingData = trendingAnime()
+    animeData = ratingsAnime()
 
     return {
-        "trending": trendingData["data"]
+        "trending": trendingData["data"],
+        "data": animeData["data"]
     }
