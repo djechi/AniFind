87c139c346f231deba973f728a88b3f1f3011136
djechi <djechipare@gmail.com>
2025-11-18 15:40:29 -0800

    Added CORS to allow communication from flask with REACT

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from homePage import homeBP
 from recommendPage import recommendBP
 
 app = Flask(__name__)
+CORS(app, resources={r"/": {"origins": ""}})
 
 #Test
 @app.route("/helloWorld")
