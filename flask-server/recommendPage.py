9140c83a81db85a04db53c224cb314cf761b675f
Bnmpower <bnmpowerislove@gmail.com>
2025-11-18 15:36:16 -0800

    Created search query and return JSON data

--- flask-server/recommendPage.py
+++ flask-server/recommendPage.py
@@ -1,17 +1,23 @@
-from flask import Blueprint
-import requests
+from flask import Blueprint, request, requests
 
-recommendBP = Blueprint("recommendBP",__name__)
+recommendBP = Blueprint("recommendBP", __name__)
 restAPI = "https://api.jikan.moe/v4/"
 
 def recommendedAnime():
     urlRecommended = restAPI + "recommendations/anime"
     return requests.get(urlRecommended).json()
 
+def searchAnime(query):
+    urlSearch = f"{restAPI}anime?q={query}"
+    return requests.get(urlSearch).json()
+
 @recommendBP.route("/recommend", methods=["GET"])
 def recommendPage():
     recommendedAnimeData = recommendedAnime()
+    query = request.args.get("q")
+    searchResults = searchAnime(query)["data"]
 
     return {
-        "recommendations": recommendedAnimeData["data"],
+        "Recommendations": recommendedAnimeData["data"],
+        "Search Result": searchResults
     }
