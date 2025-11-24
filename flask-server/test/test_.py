bd86f44666600af153646f74f11023c422534f29
Bnmpower <bnmpowerislove@gmail.com>
2025-11-23 21:38:23 -0800

    Created Unit test for home page

---
+++ flask-server/test/test_.py
@@ -0,0 +1,28 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from server import app
+from unittest.mock import patch
+import pytest
+
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+@patch("homePage.requests.get")
+def test_home_page(mock_get, client):
+    mock_get.return_value.json.side_effect = [
+        {"data": [{"title": "Trending #1"}]},
+        {"data": [{"title": "Top Rated #1"}]},
+    ]
+
+    response = client.get("/home")
+    assert response.status_code == 200
+
+    data = response.get_json()
+    assert data["trending"][0]["title"] == "Trending #1"
+    assert data["rating"][0]["title"] == "Top Rated #1"
+
+    assert mock_get.call_count == 2
