eebce931e04d66cceaa5bb1533a7754497d81abc
djechi <djechipare@gmail.com>
2025-12-04 11:57:51 -0800

    Minor changes

--- front-end/src/components/about/About.tsx
+++ front-end/src/components/about/About.tsx
@@ -1,21 +1,23 @@
 const AboutSection = () => {
   return (
     <div className="mt-[8vh] mb-[8vh] flex flex-col justify-center text-center items-center h-full w-full">
-      <div className="max-w-2xl px-4">
-
-        <p className="mt-[5vh] font-ani-default text-ani-white-100 text-[4vh]">
+      <div className="w-[60vw] h-[40vh]">
+        <p className="font-ani-default text-ani-white-100 text-[4vh]">
           Our Goal
         </p>
 
-        <p className="mt-[2vh] font-ani-default text-ani-grey-200 text-[3vh] text-left">
-          Anime is a diverse medium of animation from Japan that contains many different genres. 
-          Due to its sheer volume, it can be difficult for new viewers to discover anime shows and 
-          movies they would enjoy watching. This project is interesting to us as it looks to solve 
-          this issue. As such, creating a recommendation website dedicated to anime can help others 
-          explore this niche and gain insight about different anime that align with their tastes.
+        <p className="mt-[2vh] font-ani-default text-ani-grey-200 text-[3vh] text-center">
+          Anime is a diverse medium of animation from Japan that contains many
+          different genres. Due to its sheer volume, it can be difficult for new
+          viewers to discover anime shows and movies they would enjoy watching.
+          This project is interesting to us as it looks to solve this issue. As
+          such, creating a recommendation website dedicated to anime can help
+          others explore this niche and gain insight about different anime that
+          align with their tastes.
         </p>
-
-        <p className = "mt-[5vh] font-ani-default text-ani-white-100 text-[4vh]">
+      </div>
+      <div className="mt-[4vh] w-[50vw] h-[28vh]">
+        <p className="font-ani-default text-ani-white-100 text-[4vh]">
           Contributors
         </p>
 
@@ -25,7 +27,6 @@ const AboutSection = () => {
           <li> Bryce Lu </li>
           <li> Daniel Kim </li>
         </ul>
-        
       </div>
     </div>
   );
